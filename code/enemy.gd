598fdfda93bda8da6cae94a9cf00c5c38d01c2e0
Jacob Rodgers <yudjinn@jotun.asgard.lan>
2024-08-29 15:32:55 -0600

    init

---
+++ code/enemy.gd
@@ -0,0 +1,16 @@
+extends CharacterBody3D
+
+
+@export var speed: int = 2
+@export var health: int = 10
+
+@onready var path : PathFollow3D = get_parent()
+
+func _ready() -> void:
+	$Skeleton_Minion/AnimationPlayer.play("Walking_A")
+
+func _physics_process(delta: float) -> void:
+	path.set_progress(path.get_progress() + speed * delta)
+	
+	if path.get_progress_ratio() >= 0.99:
+		path.queue_free()
---
+++ code/world.gd
@@ -0,0 +1,25 @@
+extends Node3D
+
+
+@onready var enemy: PackedScene = preload("res://scenes/mobs/minon.tscn")
+
+var enemies_to_spawn : int = 3
+
+var can_spawn : bool = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	game_manager()
+
+func game_manager() -> void:
+	if enemies_to_spawn > 0 and can_spawn:
+		$SpawnTimer.start()
+		
+		var tempEnemy = enemy.instantiate()
+		$Path3D.add_child(tempEnemy)
+		enemies_to_spawn -= 1
+		can_spawn = false
+		
+func _on_spawn_timer_timeout() -> void:
+	can_spawn = true
